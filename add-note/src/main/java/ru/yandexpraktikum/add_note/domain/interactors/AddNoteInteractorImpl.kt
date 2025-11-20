20a99295bae51ac7b1ab474ae118dadda2a3641a
sergeygap <sergeygapyandex@yandex.ru>
2025-11-20 22:34:08 +0300

    complete second task

--- add-note/src/main/java/ru/yandexpraktikum/add_note/domain/interactors/AddNoteInteractorImpl.kt
+++ add-note/src/main/java/ru/yandexpraktikum/add_note/domain/interactors/AddNoteInteractorImpl.kt
@@ -2,10 +2,11 @@ package ru.yandexpraktikum.add_note.domain.interactors
 
 import ru.yandexpraktikum.core.domain.model.Note
 import ru.yandexpraktikum.core.domain.repository.NotesRepository
+import javax.inject.Inject
 
-class AddNoteInteractorImpl(
+class AddNoteInteractorImpl @Inject constructor(
     private val repository: NotesRepository
-): AddNoteInteractor {
+) : AddNoteInteractor {
     override suspend fun invoke(note: Note) {
         repository.insertNote(note)
     }
